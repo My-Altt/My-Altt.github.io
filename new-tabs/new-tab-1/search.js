0e41bffd5debbf2e559400e3c5992c696bbac3b2
My-Altt <101564533+My-Altt@users.noreply.github.com>
2022-10-05 18:36:16 -0400

    Add files via upload

---
+++ new-tabs/new-tab-1/search.js
@@ -0,0 +1,19 @@
+function toggleEngine() {
+
+  var dSearch = document.getElementById("dSearch");
+  var gSearch = document.getElementById("gSearch");
+  var logo = document.getElementById("engineLogo").src;
+  localStorage.setItem('preferredEngine', logo);
+
+  if (logo.indexOf("google.svg") != -1) {
+    document.getElementById("engineLogo").src = "duck.svg";
+    gSearch.style.display = "none";
+    dSearch.style.display = "block";
+  } else {
+    document.getElementById("engineLogo").src = "google.svg";
+    dSearch.style.display = "none";
+    gSearch.style.display = "block";
+  }
+
+  document.getElementById("textField").reset();
+}
